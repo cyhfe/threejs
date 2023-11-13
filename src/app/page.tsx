5db5f3e07827938f283531a68a7cfd7dc02cdd97
chenyuhao <cccchenyuhao@gmail.com>
2023-11-13 09:06:15 +0800

    router

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,24 @@
+"use client";
+
+import Link from "next/link";
+
 export default function Home() {
-  return <div>home</div>;
+  const links = [
+    { href: "/demo1", label: "demo1" },
+    { href: "/demo2", label: "demo2" },
+  ];
+
+  return (
+    <div>
+      <div>
+        {links.map((link) => {
+          return (
+            <div key={link.href}>
+              <Link href={link.href}>{link.label}</Link>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
