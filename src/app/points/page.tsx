cfa3fa77e7ea02d38724196699e5e6ce94b16214
chenyuhao <cccchenyuhao@gmail.com>
2023-11-16 06:54:45 +0800

    points

--- src/app/points/page.tsx
+++ src/app/points/page.tsx
@@ -10,12 +10,15 @@ import Stats from "three/examples/jsm/libs/stats.module";
 
 const createPoints = () => {
   const points = [];
+  const range = 15;
+  for (let i = 0; i < 1000000; i++) {
+    let particle = new THREE.Vector3(
+      Math.random() * range - range / 2,
+      Math.random() * range - range / 2,
+      Math.random() * range - range / 2
+    );
 
-  for (let x = -15; x < 15; x++) {
-    for (let y = -10; y < 10; y++) {
-      let point = new THREE.Vector3(x / 4, y / 4, 0);
-      points.push(point);
-    }
+    points.push(particle);
   }
 
   const colors = new Float32Array(points.length * 3);
@@ -31,6 +34,60 @@ const createPoints = () => {
 
   return geom;
 };
+
+export const createGhostTexture = () => {
+  const canvas = document.createElement("canvas");
+  canvas.width = 32;
+  canvas.height = 32;
+
+  const ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
+  // the body
+  ctx.translate(-81, -84);
+
+  ctx.fillStyle = "orange";
+  ctx.beginPath();
+  ctx.moveTo(83, 116);
+  ctx.lineTo(83, 102);
+  ctx.bezierCurveTo(83, 94, 89, 88, 97, 88);
+  ctx.bezierCurveTo(105, 88, 111, 94, 111, 102);
+  ctx.lineTo(111, 116);
+  ctx.lineTo(106.333, 111.333);
+  ctx.lineTo(101.666, 116);
+  ctx.lineTo(97, 111.333);
+  ctx.lineTo(92.333, 116);
+  ctx.lineTo(87.666, 111.333);
+  ctx.lineTo(83, 116);
+  ctx.fill();
+
+  // the eyes
+  ctx.fillStyle = "white";
+  ctx.beginPath();
+  ctx.moveTo(91, 96);
+  ctx.bezierCurveTo(88, 96, 87, 99, 87, 101);
+  ctx.bezierCurveTo(87, 103, 88, 106, 91, 106);
+  ctx.bezierCurveTo(94, 106, 95, 103, 95, 101);
+  ctx.bezierCurveTo(95, 99, 94, 96, 91, 96);
+  ctx.moveTo(103, 96);
+  ctx.bezierCurveTo(100, 96, 99, 99, 99, 101);
+  ctx.bezierCurveTo(99, 103, 100, 106, 103, 106);
+  ctx.bezierCurveTo(106, 106, 107, 103, 107, 101);
+  ctx.bezierCurveTo(107, 99, 106, 96, 103, 96);
+  ctx.fill();
+
+  // the pupils
+  ctx.fillStyle = "blue";
+  ctx.beginPath();
+  ctx.arc(101, 102, 2, 0, Math.PI * 2, true);
+  ctx.fill();
+  ctx.beginPath();
+  ctx.arc(89, 102, 2, 0, Math.PI * 2, true);
+  ctx.fill();
+
+  const texture = new THREE.Texture(canvas);
+  texture.needsUpdate = true;
+  return texture;
+};
+
 async function init(container: HTMLDivElement) {
   const width = container.clientWidth;
   const height = container.clientHeight;
@@ -39,7 +96,7 @@ async function init(container: HTMLDivElement) {
   const renderer = new THREE.WebGLRenderer({ antialias: true });
   renderer.setPixelRatio(window.devicePixelRatio);
   renderer.setSize(width, height);
-  // renderer.setClearColor("#e2e8f0");
+  renderer.setClearColor("#e2e8f0");
   container.appendChild(renderer.domElement);
 
   renderer.shadowMap.enabled = true;
@@ -75,13 +132,11 @@ async function init(container: HTMLDivElement) {
   // scene.add(ambient);
 
   // points
-  // const material = new THREE.SpriteMaterial({ color: "red" });
-  // const sprite = new THREE.Sprite(material);
-  // scene.add(sprite);
   const material = new THREE.PointsMaterial({
     size: 0.1,
     vertexColors: true,
     color: 0xffffff,
+    map: createGhostTexture(),
   });
   const points = new THREE.Points(createPoints(), material);
   scene.add(points);
